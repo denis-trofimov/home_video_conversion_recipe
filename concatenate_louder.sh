da8387a5f3b9eb4555f4d386db898e3f5a04bca3
Denis Trofimov <silaradost@yandex.ru>
2019-01-19 19:35:16 +0300

    feat: make audio stream louder for 12 dB.

---
+++ concatenate_louder.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash #lends you some flexibility on different systems
+# Usage `basename $0` source#1 {source#2 ..  source#n} last_source target
+
+if [[ ($# < 3) || ($1 == '-h') || ($1 == '--help') || ($1 == '/help') ]]
+then
+    echo "Usage `basename $0` source#1 {source#2 ..  source#n} last_source target"
+else
+# main branch
+arg=($*)
+# There are ${arg[0]} .. ${arg[$#-2]} sources and one target ${arg[$#-1]}
+concat=concat:
+for ((i=0; i<$#-1; i++))
+do
+    echo ${arg[${i}]}
+    if [ ${i} == 0 ]
+    then
+        concat="${concat}temp${i}"
+    else
+        concat="${concat}|temp${i}"
+    fi
+    # use named pipes to avoid creating intermediate files
+    mkfifo temp${i}
+    (ffmpeg -y -i ${arg[$i]} -vcodec copy -acodec aac -b:a 128k -af "volume=12dB" -bsf:v h264_mp4toannexb -f mpegts temp${i} 2> /dev/null &)
+done
+concat="${concat}"
+ffmpeg -v verbose -y -f mpegts -i ${concat} -c copy -bsf:a aac_adtstoasc -movflags faststart ${arg[$#-1]}
+
+# Delete pipes
+for ((i=0; i<$#-1; i++))
+do
+    rm temp${i}
+done
+# end of main branch
+fi
